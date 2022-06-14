81f0ed8722077ce739a79e7ef45abf512cc1a373
cschafer26 <c.schaefer.home@gmail.com>
2022-06-14 14:10:50 +0200

    Add lang embedding.

--- models/forward_tacotron.py
+++ models/forward_tacotron.py
@@ -15,9 +15,10 @@ class SeriesPredictor(nn.Module):
 
     def __init__(self, num_chars, emb_dim=64, conv_dims=256, rnn_dims=64, dropout=0.5, semb_dims=256):
         super().__init__()
+        self.lang_embedding = nn.Embedding(2, 32)
         self.embedding = Embedding(num_chars, emb_dim)
         self.convs = torch.nn.ModuleList([
-            BatchNormConv(emb_dim + semb_dims, conv_dims, 5, relu=True),
+            BatchNormConv(emb_dim + semb_dims + 32, conv_dims, 5, relu=True),
             BatchNormConv(conv_dims, conv_dims, 5, relu=True),
             BatchNormConv(conv_dims, conv_dims, 5, relu=True),
         ])
@@ -27,12 +28,14 @@ class SeriesPredictor(nn.Module):
 
     def forward(self,
                 x: torch.Tensor,
+                lang_ind: torch.Tensor,
                 semb: torch.Tensor,
                 alpha: float = 1.0) -> torch.Tensor:
         x = self.embedding(x)
+        lemb = self.lang_embedding(lang_ind)
         speaker_emb = semb[:, None, :]
         speaker_emb = speaker_emb.repeat(1, x.shape[1], 1)
-        x = torch.cat([x, speaker_emb], dim=2)
+        x = torch.cat([x, lemb, speaker_emb], dim=2)
         x = x.transpose(1, 2)
         for conv in self.convs:
             x = conv(x)
@@ -110,9 +113,9 @@ class ForwardTacotron(nn.Module):
                                            rnn_dims=energy_rnn_dims,
                                            dropout=energy_dropout)
         self.prenet = CBHG(K=prenet_k,
-                           in_channels=embed_dims,
+                           in_channels=embed_dims + 32,
                            channels=prenet_dims,
-                           proj_channels=[prenet_dims, embed_dims],
+                           proj_channels=[prenet_dims, embed_dims + 32],
                            num_highways=prenet_num_highways,
                            dropout=prenet_dropout)
         self.lstm = nn.LSTM(2 * prenet_dims + semb_dims,
@@ -130,6 +133,7 @@ class ForwardTacotron(nn.Module):
         self.post_proj = nn.Linear(2 * postnet_dims, n_mels, bias=False)
         self.pitch_strength = pitch_strength
         self.energy_strength = energy_strength
+        self.lang_embedding = nn.Embedding(num_embeddings=2, embedding_dim=32)
         self.pitch_proj = nn.Conv1d(1, 2 * prenet_dims + semb_dims, kernel_size=3, padding=1)
         self.energy_proj = nn.Conv1d(1, 2 * prenet_dims + semb_dims, kernel_size=3, padding=1)
         for speaker_name in speaker_names:
@@ -147,15 +151,22 @@ class ForwardTacotron(nn.Module):
         mel_lens = batch['mel_len']
         pitch = batch['pitch'].unsqueeze(1)
         energy = batch['energy'].unsqueeze(1)
+        lang_ind = torch.zeros(x.size(), dtype=torch.long, device=x.device)
+        for b in range(x.size(0)):
+            item_id = batch['item_id'][b]
+            if str(item_id).startswith('p'):
+                lang_ind[b, :] = 1
 
         if self.training:
             self.step += 1
 
-        dur_hat = self.dur_pred(x, semb).squeeze(-1)
-        pitch_hat = self.pitch_pred(x, semb).transpose(1, 2)
-        energy_hat = self.energy_pred(x, semb).transpose(1, 2)
+        dur_hat = self.dur_pred(x, lang_ind, semb).squeeze(-1)
+        pitch_hat = self.pitch_pred(x, lang_ind, semb).transpose(1, 2)
+        energy_hat = self.energy_pred(x, lang_ind, semb).transpose(1, 2)
 
+        lemb = self.lang_embedding(lang_ind)
         x = self.embedding(x)
+        x = torch.cat([x, lemb], dim=-1)
         x = x.transpose(1, 2)
         x = self.prenet(x)
         speaker_emb = semb[:, None, :]
@@ -194,23 +205,27 @@ class ForwardTacotron(nn.Module):
 
     def generate(self,
                  x: torch.Tensor,
-                 semb: torch.Tensor,
+                 lang_ind: int = 0,
+                 semb: torch.Tensor = None,
                  alpha=1.0,
                  pitch_function: Callable[[torch.Tensor], torch.Tensor] = lambda x: x,
                  energy_function: Callable[[torch.Tensor], torch.Tensor] = lambda x: x) -> Dict[str, torch.Tensor]:
         self.eval()
         with torch.no_grad():
-            dur_hat = self.dur_pred(x, semb, alpha=alpha)
+            lang_inds = torch.full(x.size(), fill_value=lang_ind, device=x.device).long()
+            dur_hat = self.dur_pred(x, lang_inds, semb, alpha=alpha)
             dur_hat = dur_hat.squeeze(2)
             if torch.sum(dur_hat.long()) <= 0:
                 torch.fill_(dur_hat, value=2.)
-            pitch_hat = self.pitch_pred(x, semb).transpose(1, 2)
+            pitch_hat = self.pitch_pred(x, lang_inds, semb).transpose(1, 2)
             pitch_hat = pitch_function(pitch_hat)
-            energy_hat = self.energy_pred(x, semb).transpose(1, 2)
+            energy_hat = self.energy_pred(x, lang_inds, semb).transpose(1, 2)
             energy_hat = energy_function(energy_hat)
             return self._generate_mel(x=x, dur_hat=dur_hat,
                                       pitch_hat=pitch_hat,
-                                      energy_hat=energy_hat, semb=semb)
+                                      energy_hat=energy_hat,
+                                      lang_inds=lang_inds,
+                                      semb=semb)
 
     @torch.jit.export
     def generate_jit(self,
@@ -233,11 +248,14 @@ class ForwardTacotron(nn.Module):
 
     def _generate_mel(self,
                       x: torch.Tensor,
+                      lang_inds: torch.Tensor,
                       semb: torch.Tensor,
                       dur_hat: torch.Tensor,
                       pitch_hat: torch.Tensor,
                       energy_hat: torch.Tensor) -> Dict[str, torch.Tensor]:
+        lemb = self.lang_embedding(lang_inds)
         x = self.embedding(x)
+        x = torch.cat([x, lemb], dim=-1)
         x = x.transpose(1, 2)
         x = self.prenet(x)
         speaker_emb = semb[:, None, :]
